8f833c944e9f6c23682c725752ec42a7e7c1247d
Hugo Ch <charrade.hugo@gmail.com>
2026-07-29 17:44:08 +0200

    test: stabilize Nuxt unit test manifest

---
+++ apps/web/tests/unit/stubs/app-manifest.ts
@@ -0,0 +1,9 @@
+export const id = 'test'
+export const prerendered: string[] = []
+export const timestamp = 0
+
+export default {
+  id,
+  prerendered,
+  timestamp
+}
--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,6 +1,12 @@
 import { defineVitestConfig } from '@nuxt/test-utils/config'
+import { fileURLToPath } from 'node:url'
 
 export default defineVitestConfig({
+  resolve: {
+    alias: {
+      '#app-manifest': fileURLToPath(new URL('./tests/unit/stubs/app-manifest.ts', import.meta.url))
+    }
+  },
   test: {
     environment: 'nuxt',
     include: ['tests/unit/**/*.spec.ts']
